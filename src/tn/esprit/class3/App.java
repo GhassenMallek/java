241f4bc828464c94c38f94667aab490e24cffc41
Ghassen Mallek <mallek.ghassen@esprit.tn>
2024-10-01 15:00:49 +0100

    initial commit

---
+++ src/tn/esprit/class3/App.java
@@ -0,0 +1,11 @@
+package tn.esprit.class3;
+
+import java.util.List;
+
+public class App {
+    List a;
+    String x;
+    public static void main(String[] args)  {
+        System.out.println("Hello, World!");
+    }
+}
